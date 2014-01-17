4051a021685c889f90ddb0535b4bbb8b6b19f45b
apratimbh <apratimbh@gmail.com>
2014-01-17 22:05:19 +0530

    here

--- funcat/src/funcat/inde_set_new.java
+++ funcat/src/funcat/inde_set_new.java
@@ -35,7 +35,7 @@ import FordFulkerson.FlowNetwork;
 import FordFulkerson.StdOut;
 import FordFulkerson.FlowEdge;
 
-public class inde_set {
+public class inde_set_new {
 	public double[][] result=null,flabels=null,test_data=null;
 	public double[][] pr_val=new double[10][3];
 	OWLOntologyManager manager;
@@ -46,7 +46,7 @@ public class inde_set {
 
 	public static void main(String[] args) throws OWLOntologyCreationException
 	{
-		inde_set o=new inde_set();
+		inde_set_new o=new inde_set_new();
 		o.main();
 	}
 
@@ -70,15 +70,15 @@ public class inde_set {
 		factory = manager.getOWLDataFactory();
 
 		test_data=load_test_data("E:/dataset/cellcycle_FUN_test_expanded.arff",77);
-		double limit=0.0,current=0.9;
-		while(current>limit)
+		int limit=20,current=1;
+		while(current<limit)
 		{
 			double tp=0;
 			double fp=0;
 			double fn=0;
 			for(int test_ex_no=1;test_ex_no<result[0].length;test_ex_no++)
 			{
-				int vertex_num=2;
+				
 				// load the weights of each label
 				double[] weights=new double[result.length];
 				ArrayList<String> selected=null;
@@ -103,6 +103,7 @@ public class inde_set {
 				{
 					weights[i]=(weights[i]-min)/(max-min);
 				}
+
 				/*double[] tmp_weights=new double[vertex.size()];
 				for(int i=0;i<weights.length;i++)
 				{
@@ -113,146 +114,160 @@ public class inde_set {
 					weights[i]=tmp_weights[i];
 				}*/
 				// list of all nodes with weights above current
-				ArrayList<String> nvertex=get_vertex(vertex,weights,current);
-				// list of vertices in the network
-				ArrayList<vertex> vertex_list=new ArrayList<vertex>();
-				// list of edges in the network
-				ArrayList<edge> edge_list=new ArrayList<edge>();
-				// create the source and destination vertices
-				vertex vs=new vertex("s",2,0);
-				vertex vd=new vertex("d",1,1);
-				vertex_list.add(vs);
-				vertex_list.add(vd);
-				// each node corresponds to two vertices with an edge between them
-				for(String v : nvertex)
-				{
-					vertex v1=new vertex(v,1,vertex_num++);
-					vertex v2=new vertex(v,2,vertex_num++);
-					vertex_list.add(v1);
-					vertex_list.add(v2);
-					// lower-bound: w and capacity; inf.
-					edge_list.add(new edge(v1,v2,weights[vertex.indexOf(v)],999999));
-				}
-				// we need edges between parents and child nodes
-				for(String v1: nvertex)
-				{
-					OWLClass c1=factory.getOWLClass(IRI.create(go.getOntologyID().getOntologyIRI().toString()+"#"+v1));
-					for(String v2 : nvertex)
-					{
-						OWLClass c2=factory.getOWLClass(IRI.create(go.getOntologyID().getOntologyIRI().toString()+"#"+v2));
-						OWLAxiom axiom=factory.getOWLSubClassOfAxiom(c2, c1);
-						if(reasoner.isEntailed(axiom))
-						{
-							vertex v1l=null,v2e=null;
-							for(vertex v : vertex_list)
-							{
-								if(v.name==v1&&v.type==2)
-								{
-									v1l=v;
-								}
-								else if(v.name==v2&&v.type==1)
-								{
-									v2e=v;
-								}
-							}
-							// lower-bound: 0 and capacity; inf.
-							edge_list.add(new edge(v1l,v2e,0,999999));
-						}
-					}
-				}
-				// --check which vertices do not have incoming edges
-				ArrayList<String> has_in_e=new ArrayList<String>();
-				for(edge e : edge_list)
-				{
-					if(e.v2.type==1)
-						has_in_e.add(e.v2.name);
-				}
-				ArrayList<String> max_v=new ArrayList<String>();
-				for(String v : nvertex)
-				{
-					if(!has_in_e.contains(v))
-					{
-						max_v.add(v);
-					}
-				}
-				// -- check which vertices do not have out-going edges
-				ArrayList<String> has_out_e=new ArrayList<String>();
-				for(edge e : edge_list)
-				{
-					if(e.v1.type==2)
-						has_out_e.add(e.v1.name);
-				}
-				ArrayList<String> min_v=new ArrayList<String>();
-				for(String v : nvertex)
-				{
-					if(!has_out_e.contains(v))
-					{
-						min_v.add(v);
-					}
-				}
-				// nodes without incoming vertices have a new edge added: source-node
-				for(String v : max_v )
-				{
-					vertex vi=null;
-					for(vertex vv : vertex_list)
-					{
-						if(vv.name==v&&vv.type==1)
-						{
-							vi=vv;
-						}
-					}
-					edge_list.add(new edge(vs,vi,0,999999));
-				}
-				// nodes without outgoing vertices have a new edge added: node-destination
-				for(String v : min_v )
-				{
-					vertex vi=null;
-					for(vertex vv : vertex_list)
-					{
-						if(vv.name==v&&vv.type==2)
-						{
-							vi=vv;
-						}
-					}
-					if(vi==null)
-					{
-						System.out.println("Error: "+min_v.size()+" "+max_v.size());
-						System.exit(0);
-					}
-					edge_list.add(new edge(vi,vd,0,999999));
-				}
-
-				// As we need to solve the max-cut problem, we swap the roles of the source and destination nodes
-				Collections.swap(vertex_list, 0, 1);
-				vertex_list.get(0).num=0;
-				vertex_list.get(1).num=1;
-				vertex_list.get(0).name="s";
-				vertex_list.get(1).name="t";
-				ArrayList<String> min_cut=new ArrayList<String>();
 				selected=new ArrayList<String>();
-				// use modified FOrdFulkerson to solve the max-cut problem
-				Fordfulkerson f=new Fordfulkerson();
-				for(vertex vi : f.construct_r_graph(vertex_list, edge_list))
+				int k=0;
+				int num=current;
+				main_loop: while(true)
 				{
-					min_cut.add(vi.name);
-				}
-				// choose those nodes whose only one vertex (v1'-v2') is in the cut
-				for(int i=0;i<min_cut.size();i++)
-				{
-					if(min_cut.get(i)!="s")
+					ArrayList<String> nvertex=get_vertex(vertex,weights,num);
+					// list of vertices in the network
+					ArrayList<vertex> vertex_list=new ArrayList<vertex>();
+					// list of edges in the network
+					ArrayList<edge> edge_list=new ArrayList<edge>();
+					// create the source and destination vertices
+					vertex vs=new vertex("s",2,0);
+					vertex vd=new vertex("d",1,1);
+					vertex_list.add(vs);
+					vertex_list.add(vd);
+					// each node corresponds to two vertices with an edge between them
+					int vertex_num=2;
+					for(String v : nvertex)
 					{
-						boolean flag=true;
-						for(int j=0;j<min_cut.size();j++)
+						vertex v1=new vertex(v,1,vertex_num++);
+						vertex v2=new vertex(v,2,vertex_num++);
+						vertex_list.add(v1);
+						vertex_list.add(v2);
+						// lower-bound: w and capacity; inf.
+						edge_list.add(new edge(v1,v2,weights[vertex.indexOf(v)],999999));
+					}
+					// we need edges between parents and child nodes
+					for(String v1: nvertex)
+					{
+						OWLClass c1=factory.getOWLClass(IRI.create(go.getOntologyID().getOntologyIRI().toString()+"#"+v1));
+						for(String v2 : nvertex)
 						{
-							if((i!=j)&&(min_cut.get(i)==min_cut.get(j)))
+							OWLClass c2=factory.getOWLClass(IRI.create(go.getOntologyID().getOntologyIRI().toString()+"#"+v2));
+							OWLAxiom axiom=factory.getOWLSubClassOfAxiom(c2, c1);
+							if(reasoner.isEntailed(axiom))
 							{
-								flag=false;
+								vertex v1l=null,v2e=null;
+								for(vertex v : vertex_list)
+								{
+									if(v.name==v1&&v.type==2)
+									{
+										v1l=v;
+									}
+									else if(v.name==v2&&v.type==1)
+									{
+										v2e=v;
+									}
+								}
+								// lower-bound: 0 and capacity; inf.
+								edge_list.add(new edge(v1l,v2e,0,999999));
 							}
 						}
-						if(flag)
+					}
+					// --check which vertices do not have incoming edges
+					ArrayList<String> has_in_e=new ArrayList<String>();
+					for(edge e : edge_list)
+					{
+						if(e.v2.type==1)
+							has_in_e.add(e.v2.name);
+					}
+					ArrayList<String> max_v=new ArrayList<String>();
+					for(String v : nvertex)
+					{
+						if(!has_in_e.contains(v))
 						{
-							selected.add(min_cut.get(i));
+							max_v.add(v);
 						}
+					}
+					// -- check which vertices do not have out-going edges
+					ArrayList<String> has_out_e=new ArrayList<String>();
+					for(edge e : edge_list)
+					{
+						if(e.v1.type==2)
+							has_out_e.add(e.v1.name);
+					}
+					ArrayList<String> min_v=new ArrayList<String>();
+					for(String v : nvertex)
+					{
+						if(!has_out_e.contains(v))
+						{
+							min_v.add(v);
+						}
+					}
+					// nodes without incoming vertices have a new edge added: source-node
+					for(String v : max_v )
+					{
+						vertex vi=null;
+						for(vertex vv : vertex_list)
+						{
+							if(vv.name==v&&vv.type==1)
+							{
+								vi=vv;
+							}
+						}
+						edge_list.add(new edge(vs,vi,0,999999));
+					}
+					// nodes without outgoing vertices have a new edge added: node-destination
+					for(String v : min_v )
+					{
+						vertex vi=null;
+						for(vertex vv : vertex_list)
+						{
+							if(vv.name==v&&vv.type==2)
+							{
+								vi=vv;
+							}
+						}
+						if(vi==null)
+						{
+							System.out.println("Her: Error: "+min_v.size()+" "+max_v.size());
+							System.exit(0);
+						}
+						edge_list.add(new edge(vi,vd,0,999999));
+					}
+
+					// As we need to solve the max-cut problem, we swap the roles of the source and destination nodes
+					Collections.swap(vertex_list, 0, 1);
+					vertex_list.get(0).num=0;
+					vertex_list.get(1).num=1;
+					vertex_list.get(0).name="s";
+					vertex_list.get(1).name="t";
+					ArrayList<String> min_cut=new ArrayList<String>();
+					// use modified FOrdFulkerson to solve the max-cut problem
+					Fordfulkerson f=new Fordfulkerson();
+					for(vertex vi : f.construct_r_graph(vertex_list, edge_list))
+					{
+						min_cut.add(vi.name);
+					}
+					// choose those nodes whose only one vertex (v1'-v2') is in the cut
+					for(int i=0;i<min_cut.size();i++)
+					{
+						if(min_cut.get(i)!="s")
+						{
+							boolean flag=true;
+							for(int j=0;j<min_cut.size();j++)
+							{
+								if((i!=j)&&(min_cut.get(i)==min_cut.get(j)))
+								{
+									flag=false;
+								}
+							}
+							if(flag)
+							{
+								selected.add(min_cut.get(i));
+							}
+						}
+					}
+					if(selected.size()<current)
+					{
+						num++;
+					}
+					else 
+					{
+						break main_loop;
 					}
 				}
 				/*for(String s : selected)
@@ -351,7 +366,7 @@ public class inde_set {
 			} // --for loop end // all examples
 
 			System.out.println("Current-limit--"+current+"\n");
-			current-=0.02;
+			current++;
 
 			System.out.println("Precision: "+tp/(tp+fp));
 			System.out.println("Recall: "+tp/(tp+fn));
@@ -393,15 +408,45 @@ public class inde_set {
 		}
 		return temp1;
 	}
-	public ArrayList<String> get_vertex(ArrayList<String> vertex,double[] weights,double w)
+	
+	public void sort(ArrayList<String> vertex,double[] weights,int c)
+	{
+		for(int i=0;i<c;i++)
+		{
+			double max=weights[i];
+			int index=i;
+			for(int j=i;j<weights.length;j++)
+			{
+				if(weights[j]>max)
+				{
+					max=weights[j];
+					index=j;
+				}
+			}
+			double tmp=weights[i];
+			weights[i]=weights[index];
+			weights[index]=tmp;
+			Collections.swap(vertex, i, index);
+		}
+	}
+	
+	public ArrayList<String> get_vertex(ArrayList<String> vertex,double[] weights,int num)
 	{
 		ArrayList<String> tmp=new ArrayList<String>();
+		double[] tmp_weights=new double[weights.length];
+		ArrayList<String> tmp_vertex=new ArrayList<String>();
 		for(int i=0;i<weights.length;i++)
 		{
-			if(weights[i]>=w)
-			{
-				tmp.add(vertex.get(i));
-			}
+			tmp_weights[i]=weights[i];
+		}
+		for(String s: vertex)
+		{
+			tmp_vertex.add(s);
+		}
+		sort(tmp_vertex,weights,num);
+		for(int i=0;i<num;i++)
+		{
+			tmp.add(tmp_vertex.get(i));
 		}
 		return tmp;
 
